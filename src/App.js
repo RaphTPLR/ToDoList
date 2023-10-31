08279603602023e96cadf3a90d5d535bdfa71014
RaphTPLR <102389757+RaphTPLR@users.noreply.github.com>
2023-10-31 14:17:12 +0100

    ci: structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Link, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home'
 
 function App() {
   return (
     <div className="App">
-      <p>Hello World !</p>
+      <Link to="/">Accueil</Link>
+
+      <Routes>
+          <Route path='/' element = { <Home/> }/>
+        </Routes>
     </div>
   );
 }
